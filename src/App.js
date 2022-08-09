c6c94fd78137d87071e3ce67ac7e3169c5564985
juhye <mari5669@naver.com>
2022-08-09 23:47:26 +0900

    Market BApp: NFT발행

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import "./market.css";
 import QRCode from 'qrcode.react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { getBalance, readCount, setCount } from './api/UseCaver';
-import { useState } from 'react';
+import { getBalance, fetchCardsOf } from './api/UseCaver';
+import React, { useState } from 'react';
 import * as KlipAPI from './api/UseKlip';
-import {Alert, Container} from 'react-bootstrap';
+import {Alert, Card, Container, Nav, Form, Button} from 'react-bootstrap';
+import { MARKET_CONTRACT_ADDRESS } from './constants';
 
 
 const DEFAULT_QR_CODE = "DEFAULT";
@@ -16,20 +17,39 @@ function App() {
   // Global Data (domain data)
   //address
   // nft
-  const [nfts, setNfts] = useState([]);
+  const [nfts, setNfts] = useState([]); //{tokenId: '101', tokenUri: ''}
   const [myBalance, setMyBalance] = useState('0');
   const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
 
   //UI
   const [qrvalue, setQrvalue] = useState(DEFAULT_QR_CODE);
+  const [tab, setTab] = useState('MINT') //market, mint, wallet
+  const [mintImageUrl, setMintImageUrl] = useState("");
   //tab
   //mintInput
 
   //Modal
 
   //fetchMarketNFTs
+  const fetchMarketNFTs = async ()=> {
+    const _nfts = await fetchCardsOf(MARKET_CONTRACT_ADDRESS);
+    setNfts(_nfts);
+  }
+
   //fetchMyNFTs
+  const fetchMyNFTs = async ()=> {
+    const _nfts = await fetchCardsOf('0xD9af7D7E78E11D97DD84BE78CdFa3C2BCaA25A14');
+    setNfts(_nfts);
+  }
+
   //onClickMint
+  const onClickMint = async (uri)=> {
+    if(myAddress === DEFAULT_ADDRESS) alert("NO ADRESS");
+    const randomTokenId = parseInt(Math.random()*1000000);
+    KlipAPI.mintCardWithURI(myAddress,randomTokenId, uri, setQrvalue, (result)=> {
+      alert(JSON.stringify(result));
+    });
+  };
   //onClickMyCard
   //onClickMarketCard
 
@@ -44,6 +64,7 @@ function App() {
   return (
     <div className="App">
       <div style = {{backgroundColor: "#B6D7A8" , padding: 10}}>
+        {/*주소 잔고*/}
         <div 
         style = {{
           fontSize: 30, 
@@ -64,22 +85,97 @@ function App() {
           >
             {myBalance}
           </Alert>
-        </div>
-        {/*주소 잔고*/}
-        <Container 
+          <Container 
         style = {{
           backgroundColor: "white",
           width: 300, 
           height: 300, 
           padding: 20,
          }}>
-          <QRCode value={qrvalue} size={256} style ={{margin: "auto"}}/>
+        <QRCode value={qrvalue} size={256} style ={{margin: "auto"}}/>
+        <br/>
+        <br/>
         </Container>
-        
-        {/*갤러리*/}
+
+          {/*갤러리(마켓, 내 지갑) */}
+          {tab ==="MARKET" || tab === "WALLET" ? (
+             <div className='container' style = {{padding:0, width: "100%"}}>
+            {nfts.map((nft,index)=> (
+              <Card.Img className='img-responsive' src={nfts[index].uri}></Card.Img>
+            ))}
+            </div>
+             ) :null }
         {/*발행 페이지*/}
-        {/*탭*/}
+        {tab === "MINT" ?  (
+        <div className='container' style={{padding:0, width: "100%"}}>
+          <Card className='text-center' style={{color:"black", height:"50%",borderColor:"#C5B358" }}>
+            <Card.Body style={{opacity: 0.9, backgroundColor:"black" }}>
+              {mintImageUrl !== ""? <Card.Img src={mintImageUrl} height={"50%"} />:null}
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                  value={mintImageUrl}
+                  onChange={(e)=> {
+                    console.log(e.target.value);
+                    setMintImageUrl(e.target.value);
+                  }}
+                  type="text"
+                  placeholder="이미지 주소를 입력해주세요"
+                  />
+                </Form.Group>
+                <br/>
+                <Button
+                onClick={()=>{
+                  onClickMint(mintImageUrl)}} 
+                variant="primary" 
+                style={{
+                  backgroundColor:"#B10034",
+                  borderColor: "B10034"
+                  }}> 발행하기
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+
+          </div> ): null}
+        </div>
+        <button onClick={fetchMyNFTs}>
+         NFT 가져오기
+        </button>
+
         {/*모달*/}
+        {/*탭*/}
+        <nav style ={{backgroundColor:"#1b1717", height: 45}} 
+          className="navbar fixed-bottom navbar-light"
+          role="navigation">
+          <Nav className="w-100">
+            <div className='d-flex flex-row justify-content-around w-100'>
+              <div onClick={()=> {
+                setTab("MARKET");
+                fetchMarketNFTs();
+              }}
+              className = "row d-flex flex-column justify-content-center align-items-center"
+            >
+              <div>MARKET</div>
+            </div>
+            <div onClick={()=> {
+                setTab("MINT");
+              }}
+              className = "row d-flex flex-column justify-content-center align-items-center"
+            >
+              <div>MINT</div>
+            </div>
+            <div onClick={()=> {
+                setTab("WALLET");
+                fetchMyNFTs();
+              }}
+              className = "row d-flex flex-column justify-content-center align-items-center"
+            >
+              <div>WALLET</div>
+            </div>
+          </div>
+          </Nav>
+        </nav>
     </div>
   );
 }
--- src/api/UseCaver.js
+++ src/api/UseCaver.js
@@ -1,6 +1,7 @@
 import Caver from 'caver-js';
-import CounterABI from '../abi/CounterABI.json';
-import {ACCESS_KEY_ID, SECRET_ACCESS_KEY, COUNT_CONTRACT_ADDRESS, CHAIN_ID} from '../constants';
+import KIP17ABI from '../abi/KIP17TokenABI.json';
+import MarketABI from '../abi/MarketABI.json';
+import {ACCESS_KEY_ID, SECRET_ACCESS_KEY, CHAIN_ID, NFT_CONTRACT_ADDRESS} from '../constants';
 
 const option = {
     headers:[
@@ -13,12 +14,34 @@ const option = {
   }
   
   const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
-  const CountContract = new caver.contract(CounterABI, COUNT_CONTRACT_ADDRESS);
-  
-  export const readCount = async () => {
-    const _count = await CountContract.methods.count().call();
-    console.log(_count);
-  }
+  const NFTContract = new caver.contract(KIP17ABI, NFT_CONTRACT_ADDRESS);
+
+
+  export const fetchCardsOf = async (address)=> {
+    //fetch Balance
+    const balance = await NFTContract.methods.balanceOf(address).call();
+    console.log(`[NFT Balance] ${balance}`);
+    //fetch Token IDs
+
+    const tokenIds = [];
+    for (let i = 0; i< balance; i++) {
+      const id = await NFTContract.methods.tokenOfOwnerByIndex(address, i).call();
+      tokenIds.push(id);
+    }
+    //fetch Token URIs
+    const tokenUris = [];
+    for (let i = 0; i< balance; i++) {
+      const uri = await NFTContract.methods.tokenURI(tokenIds[i]) .call();
+      tokenUris.push(uri);
+    }
+    const nfts = [];
+    for(let i = 0;i<balance; i++){
+      nfts.push({uri: tokenUris[i], id:tokenIds[i]});
+    }
+    console.log(nfts);
+    return nfts;
+  };
+
   
   export const getBalance = (address) => {
     return caver.rpc.klay.getBalance(address).then((response)=> { //특정 잔고에 대한 주소를 가져다 주세요 -> 답변이 오면
@@ -27,23 +50,29 @@ const option = {
       return balance;
     })
   }
+
+  //const CountContract = new caver.contract(CounterABI, COUNT_CONTRACT_ADDRESS);
+
+  // export const readCount = async () => {
+  //   const _count = await CountContract.methods.count().call();
+  //   console.log(_count);
+  // }
   
-  export const setCount = async (newCount)=> {
-    //사용할 account 설정
-  
-    try {
-      const privatekey = '0x8808ceed2a7d75475d0be4a6e978b6629c888c3bf1ead323ac6563b86f252155';
-      const deployer = caver.wallet.keyring.createFromPrivateKey(privatekey);
-      caver.wallet.add(deployer);
-      // 스마트 컨트랙트 실행 트랜잭션 날리기
-      //결과확인
+  // export const setCount = async (newCount)=> {
+  //   //사용할 account 설정
+  //   try {
+  //     const privatekey = '0x8808ceed2a7d75475d0be4a6e978b6629c888c3bf1ead323ac6563b86f252155';
+  //     const deployer = caver.wallet.keyring.createFromPrivateKey(privatekey);
+  //     caver.wallet.add(deployer);
+  //     // 스마트 컨트랙트 실행 트랜잭션 날리기
+  //     //결과확인
     
-      const receipt = await CountContract.methods.setCount(newCount).send({
-        from: deployer.address, //address
-        gas:"0x4bfd200" //적당히 큰 수 
-      })
-      console.log(receipt);
-    } catch(e) {
-      console.log(`[ERROR_SET_COUNT]${e}`);
-    }
-  }
+  //     const receipt = await CountContract.methods.setCount(newCount).send({
+  //       from: deployer.address, //address
+  //       gas:"0x4bfd200" //적당히 큰 수 
+  //     })
+  //     console.log(receipt);
+  //   } catch(e) {
+  //     console.log(`[ERROR_SET_COUNT]${e}`);
+  //   }
+  // }
